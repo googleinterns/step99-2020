16da5fd59b81ed4f438535877c403185364c1611
Seun <seunomonije@icloud.com>
2020-06-30 16:58:40 +0000

    storage

--- server/src/main/java/com/google/musicanalysis/site/Perspective/PerspectiveServlet.java
+++ server/src/main/java/com/google/musicanalysis/site/Perspective/PerspectiveServlet.java
@@ -9,11 +9,31 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.cloud.language.v1.Document;
+import com.google.cloud.language.v1.Document.Type;
+import com.google.cloud.language.v1.LanguageServiceClient;
+import com.google.cloud.language.v1.Sentiment;
+
 @WebServlet("/api/perspective")
 public class PerspectiveServlet extends HttpServlet {
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse res)
       throws ServletException, IOException {
+
+        // Instantiates a client
+    try (LanguageServiceClient language = LanguageServiceClient.create()) {
+
+      // The text to analyze
+      String text = "Hello, world!";
+      Document doc = Document.newBuilder().setContent(text).setType(Type.PLAIN_TEXT).build();
+
+      // Detects the sentiment of the text
+      Sentiment sentiment = language.analyzeSentiment(doc).getDocumentSentiment();
+
+      System.out.printf("Text: %s%n", text);
+      System.out.printf("Sentiment: %s, %s%n", sentiment.getScore(), sentiment.getMagnitude());
+    }
+
     PerspectiveRequest request;
     ArrayList<String> attributes =
         new ArrayList<>(
