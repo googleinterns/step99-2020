750ef72b3bc3d2cf61ec91e262a329f9de94039b
Grace Tian <gracetian6@gmail.com>
2020-06-30 18:21:02 -0400

    polishing up youtube api

--- server/src/main/java/com/google/musicanalysis/site/YoutubeServlet.java
+++ server/src/main/java/com/google/musicanalysis/site/YoutubeServlet.java
@@ -4,6 +4,9 @@
  * https://developers.google.com/explorer-help/guides/code_samples#java
  */
 package com.google.musicanalysis.site;
+import com.google.musicanalysis.util.Secrets;
+import com.google.musicanalysis.util.URLEncodedBuilder;
+
 
 import com.google.gson.JsonParser;
 import com.google.musicanalysis.util.URLEncodedBuilder;
@@ -27,10 +30,30 @@ public class YoutubeServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse res) 
         throws ServletException, IOException {
-        // retrieve youtube access token 
+        // don't know how to catch null pointer exception
+
+        // retrieve Youtube API key and access token 
+        String API_KEY = Secrets.getSecretString("YOUTUBE_API_KEY");
         HttpSession session = req.getSession();
-        String accessToken = (String) session.getAttribute("youtube_access_token");
-        res.getWriter().printf("YOUTUBE: %s", accessToken);
+        String accessToken = session.getAttribute("youtube_access_token").toString();
     
+        // make http request to youtube API
+        var youtubeParam = new URLEncodedBuilder()
+            .add("part", "topicDetails")
+            .add("myRating", "like")
+            .add("key", API_KEY);
+        URI youtubeUri = URI.create("https://www.googleapis.com/youtube/v3/videos?" + youtubeParam.build());
+
+        var httpClient = HttpClient.newHttpClient();
+        var youtubeReq = HttpRequest.newBuilder(youtubeUri)
+                .header("Authorization", "Bearer " + accessToken)
+                .header("Accept", "application/json")
+                .GET()
+                .build();
+
+        // get response to Youtube API 
+        var youtubeRes = httpClient.sendAsync(youtubeReq, BodyHandlers.ofString()).join();
+        var youtubeResBody = youtubeRes.body();
+        res.getWriter().write(youtubeResBody);
     }
 }
