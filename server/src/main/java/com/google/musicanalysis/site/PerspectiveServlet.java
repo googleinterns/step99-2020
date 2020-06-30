dff0762706dfe4decfd2b6a42d0565b3ef355ee0
Seun <seunomonije@icloud.com>
2020-06-30 22:44:10 +0000

    fix perspective

--- server/src/main/java/com/google/musicanalysis/site/PerspectiveServlet.java
+++ server/src/main/java/com/google/musicanalysis/site/PerspectiveServlet.java
@@ -1,9 +1,5 @@
 package com.google.musicanalysis.site;
 
-import com.google.cloud.language.v1.Document;
-import com.google.cloud.language.v1.Document.Type;
-import com.google.cloud.language.v1.LanguageServiceClient;
-import com.google.cloud.language.v1.Sentiment;
 import com.google.musicanalysis.api.perspective.*;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -19,20 +15,6 @@ public class PerspectiveServlet extends HttpServlet {
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse res)
       throws ServletException, IOException {
-
-    // Instantiates a client
-    try (LanguageServiceClient language = LanguageServiceClient.create()) {
-
-      // The text to analyze
-      String text = "Hello, world!";
-      Document doc = Document.newBuilder().setContent(text).setType(Type.PLAIN_TEXT).build();
-
-      // Detects the sentiment of the text
-      Sentiment sentiment = language.analyzeSentiment(doc).getDocumentSentiment();
-
-      System.out.printf("Text: %s%n", text);
-      System.out.printf("Sentiment: %s, %s%n", sentiment.getScore(), sentiment.getMagnitude());
-    }
 
     PerspectiveRequest request;
     ArrayList<String> attributes =
