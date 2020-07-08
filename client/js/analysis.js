a40acc864d6bb3163408616e51df8ab2032afb00
Seun <seunomonije@icloud.com>
2020-07-08 15:37:12 +0000

    resolved reqs

--- client/js/analysis.js
+++ client/js/analysis.js
@@ -1,10 +1,8 @@
 
 
-window.onload = function() {
-    // Listen for on submit click
-    let formSubmit = document.getElementById("formSubmit");
-    formSubmit.addEventListener("click", fetchAndShowResponse, false);
-}
+// Listen for on submit click
+let formSubmit = document.getElementById("formSubmit");
+formSubmit.addEventListener("click", fetchAndShowResponse, false);
 
 /**
  * Grabs response from server
@@ -12,16 +10,8 @@ window.onload = function() {
 async function fetchAndShowResponse() {
     const response = await fetch('/api/musix');
     const value = await response.json();
-    changeInnerText("response", value);
+    const el = document.getElementById(elId);
+    el.innerText = value;
     return value;
 }
 
-/**
- * Change the contents of div
- * @param elId the id of the element 
- * @param value the value to insert as innerText
- */
-function changeInnerText(elId, value) {
-    const el = document.getElementById(elId);
-    el.innerText = value;
-}
