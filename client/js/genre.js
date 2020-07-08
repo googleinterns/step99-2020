9d1f222c495b92d64976d725cebb883661711297
Grace Tian <gracetian6@gmail.com>
2020-07-08 18:54:05 -0400

    hash map shows up in console.log

--- client/js/genre.js
+++ client/js/genre.js
@@ -2,7 +2,7 @@ console.log("Display text on /api/youtube");
 
 async function getMusicGenre() {
   let response = await fetch("/api/youtube");
-  return await response;
+  return await response.text();
 }
 
 console.log(getMusicGenre());
