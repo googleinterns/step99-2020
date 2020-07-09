dcf5fe6c008b549498469098523ba074bd465079
Grace Tian <gracetian6@gmail.com>
2020-07-09 16:49:18 -0400

    [frontend] display hash map using .then

--- client/js/genre.js
+++ client/js/genre.js
@@ -1,8 +1,18 @@
 console.log("Display text on /api/youtube");
-
+/* returns a promise */
 async function getMusicGenre() {
   let response = await fetch("/api/youtube");
   return await response.text();
 }
 
 console.log(getMusicGenre());
+
+const genreBlock = document.getElementById('genres');
+getMusicGenre().then(genre => genreBlock.innerHTML = genre);
+
+/* the return value of an async function is always a promise, so you need to do
+  .then to unwrap the promise. Within the .then then you can do the desired fns
+  because you will have access to the unwrapped fn
+
+  
+ */
