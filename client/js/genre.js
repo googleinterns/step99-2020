f10526d7044274c367b0243262719443f8f8d808
Grace Tian <gracetian6@gmail.com>
2020-07-10 18:35:27 -0400

    updated comments for genre.js

--- client/js/genre.js
+++ client/js/genre.js
@@ -1,14 +1,28 @@
-/** fetches youtube api calls from YoutubeServlet.java
-    and displays on youtube-genre.html
-*/
+/**
+ * fetches youtube api calls from YoutubeServlet.java
+ * and displays on youtube-genre.html
+ */
+
 const numVideosInput = document.getElementById('numVideos');
 const DEFAULT_NUM_VIDS = 10;
 
+/**
+ * extracts num vids from input, otherwise returns default
+ * helper fn for displayMusicGenre()
+ * @returns {number} num videos user wants to retrieve
+ */
+function getNumVids() {
+  const numVids = numVideosInput.value;
+  return numVids || DEFAULT_NUM_VIDS;
+}
+
+/**
+ * fetches genre count hashmap from /api/youtube and updates html
+ */
 async function displayMusicGenre() {
   const genreBlock = document.getElementById('genres');
   const numVids = getNumVids();
   const response = await fetch(`/api/youtube?num_videos=${numVids}`);
-  console.log(response.status);
   if (response.status != 200) {
     // redirect to login if there are errors 
     window.location.href = "/api/oauth/login/youtube";
@@ -17,17 +31,16 @@ async function displayMusicGenre() {
   genreBlock.innerHTML = genreCount;
 }
 
+/**
+ * updates value in num videos input
+ * by extracting num_videos param in URL
+ * otherwise if no num_videos param does nothing
+ */
 function updateNumVids() {
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
-  console.log(queryString);
-  console.log(urlParams.get('num_videos'));
   numVideosInput.value = urlParams.get('num_videos');
 }
 
-function getNumVids() {
-  const numVids = numVideosInput.value;
-  return numVids || DEFAULT_NUM_VIDS;
-}
 updateNumVids();
 displayMusicGenre();
