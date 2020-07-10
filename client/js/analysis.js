61d4f2f9037328113501916151173654f588a211
Seun <seunomonije@icloud.com>
2020-07-10 19:57:41 +0000

    donut chart

--- client/js/analysis.js
+++ client/js/analysis.js
@@ -1,9 +1,13 @@
+// Will be overwritten in merge
 window.onload = function() {
   // Listen for on submit click
   const formSubmit = document.getElementById('formSubmit');
   formSubmit.addEventListener('click', fetchAndShowResponse, false);
 };
 
+/**
+ * Will be overwritten in merge
+ */
 async function fetchAndShowResponse() {
   const response = await fetch('/api/musix');
   const value = await response.json();
@@ -11,59 +15,72 @@ async function fetchAndShowResponse() {
   return value;
 }
 
+/**
+ * Will be overwritten in merge
+ * @param elId
+ * @param value
+ */
 function changeInnerText(elId, value) {
   const el = document.getElementById(elId);
   el.innerText = value;
 }
 
-document.body.onload = addElement;
-
+/**
+ * Adds a new chart to the page
+ */
 function addElement() {
-  var div = document.createElement("div");
-  div.className = "item donut";
-  
-  var header = document.createElement("h2");
-  header.innerText = "Toxicity";
+  const div = document.createElement('div');
+  div.className = 'item donut';
+
+  const header = document.createElement('h2');
+  header.innerText = 'Toxicity';
   div.appendChild(header);
-  
-  var svg = createSVGElement("svg");
-  svg.setAttribute("xmlns", "http://www.w3.org/2000/svg");
-  var g = buildCircle();
+
+  const svg = createSVGElement('svg');
+  svg.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
+  const g = buildCircle();
   svg.appendChild(g);
   div.appendChild(svg);
-  
-  var currentDiv = document.getElementById("poop");
+
+  const currentDiv = document.getElementById('id');
   document.body.insertBefore(div, currentDiv);
 }
 
+/**
+ * Builds the circles within the svg tag
+ */
 function buildCircle() {
-  var g = createSVGElement("g");
-  
-  var insideCircle = createSVGElement("circle");
-  
-  insideCircle.setAttribute("r", "70");
-  insideCircle.setAttribute("cx", "50%");
-  insideCircle.setAttribute("cy", "50%");
-  insideCircle.setAttribute("stroke-width", "20");
-  insideCircle.setAttribute("stroke", "#f2f2f2");
-  insideCircle.setAttribute("fill", "none");
-  
-  var outsideCircle = createSVGElement("circle");
-  
-  outsideCircle.setAttribute("class", "circle_animation");
-  outsideCircle.setAttribute("r", "70");
-  outsideCircle.setAttribute("cx", "50%");
-  outsideCircle.setAttribute("cy", "50%");
-  outsideCircle.setAttribute("stroke-width", "20");
-  outsideCircle.setAttribute("stroke", "#ffb6c1"); // light pink
-  outsideCircle.setAttribute("fill", "none");
-  
+  const g = createSVGElement('g');
+
+  const insideCircle = createSVGElement('circle');
+
+  insideCircle.setAttribute('r', '70');
+  insideCircle.setAttribute('cx', '50%');
+  insideCircle.setAttribute('cy', '50%');
+  insideCircle.setAttribute('stroke-width', '20');
+  insideCircle.setAttribute('stroke', '#f2f2f2');
+  insideCircle.setAttribute('fill', 'none');
+
+  const outsideCircle = createSVGElement('circle');
+
+  outsideCircle.setAttribute('class', 'circle_animation');
+  outsideCircle.setAttribute('r', '70');
+  outsideCircle.setAttribute('cx', '50%');
+  outsideCircle.setAttribute('cy', '50%');
+  outsideCircle.setAttribute('stroke-width', '20');
+  outsideCircle.setAttribute('stroke', '#ffb6c1'); // light pink
+  outsideCircle.setAttribute('fill', 'none');
+
   g.appendChild(insideCircle);
   g.appendChild(outsideCircle);
-  
+
   return g;
 }
 
+/**
+ * Creates an SVG Element
+ * @param el
+ */
 function createSVGElement(el) {
-  return document.createElementNS("http://www.w3.org/2000/svg", el);
+  return document.createElementNS('http://www.w3.org/2000/svg', el);
 }
