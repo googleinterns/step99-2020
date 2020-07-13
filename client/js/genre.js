36de1774112a7b4a488bba5979fcc0693ff33013
Grace Tian <gracetian6@gmail.com>
2020-07-13 11:43:08 -0400

    added comments in genre.js

--- client/js/genre.js
+++ client/js/genre.js
@@ -3,12 +3,17 @@
 */
 
 const genreBlock = document.getElementById('genres');
+
+/**
+ * fetches genre count hashmap from /api/youtube and updates html
+ */
 async function displayMusicGenre() {
   const response = await fetch("/api/youtube");
   if (response.status == 401) {
     // no oauth login so redirect
     window.location.href = "/api/oauth/login/youtube";
   }
+
   const genreCount = await response.text();
   genreBlock.innerHTML = genreCount;
 }
