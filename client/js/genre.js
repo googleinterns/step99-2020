abf9ec721b52adfd3b324860729e248fdb09347a
Grace Tian <gracetian6@gmail.com>
2020-07-15 16:47:58 -0400

    fix lint issues

--- client/js/genre.js
+++ client/js/genre.js
@@ -1,6 +1,6 @@
 /** fetches youtube api calls from YoutubeServlet.java
     and displays on youtube-genre.html
-*/
+ */
 
 const genreBlock = document.getElementById('genres');
 
@@ -8,10 +8,10 @@ const genreBlock = document.getElementById('genres');
  * fetches genre count hashmap from /api/youtube and updates html
  */
 async function displayMusicGenre() {
-  const response = await fetch("/api/youtube");
+  const response = await fetch('/api/youtube');
   if (response.status == 401) {
     // no oauth login so redirect
-    window.location.href = "/api/oauth/login/youtube";
+    window.location.href = '/api/oauth/login/youtube';
   }
 
   const genreCount = await response.text();
