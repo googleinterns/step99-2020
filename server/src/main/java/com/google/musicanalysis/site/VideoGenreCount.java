946db0406d9bef7ed4f58357cece7afd39264c95
Grace Tian <gracetian6@gmail.com>
2020-07-15 18:14:30 -0400

    updates genreCountList with VideoGenreCount obj

--- server/src/main/java/com/google/musicanalysis/site/VideoGenreCount.java
+++ server/src/main/java/com/google/musicanalysis/site/VideoGenreCount.java
@@ -2,8 +2,9 @@ package com.google.musicanalysis.site;
 
 /** contains number of youtube videos under a music genre */
 public class VideoGenreCount {
-  private final String genre;
-  private final int count;
+  public final String genre;
+  public static int count;
+
   public VideoGenreCount(String genre, int count) {
     this.genre = genre;
     this.count = count;
