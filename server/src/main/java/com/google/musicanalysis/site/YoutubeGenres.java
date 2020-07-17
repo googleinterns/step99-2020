2d19c5fb0ac2884f23582ce5d22e76e5a3caa7ee
Grace Tian <gracetian6@gmail.com>
2020-07-17 12:38:14 -0400

    added Other Music in list, totalMusic outside list

--- server/src/main/java/com/google/musicanalysis/site/YoutubeGenres.java
+++ server/src/main/java/com/google/musicanalysis/site/YoutubeGenres.java
@@ -6,9 +6,11 @@ import java.util.List;
 public class YoutubeGenres {
   private final List<VideoGenreCount> data;
   private final int totalLiked;
+  private final int totalMusic;
 
-  public YoutubeGenres(List<VideoGenreCount> genreCountList, int totalLiked) {
+  public YoutubeGenres(List<VideoGenreCount> genreCountList, int totalLiked, int totalMusic) {
     this.data = genreCountList;
     this.totalLiked = totalLiked;
+    this.totalMusic = totalMusic;
   }
 }
