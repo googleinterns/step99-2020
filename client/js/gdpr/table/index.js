370cd50cbcb82ac94dcc959e22cde4073b704816
Ibiyemi Abiodun <ibiyemi@google.com>
2020-07-20 18:50:56 -0400

    Add GDPR Table

--- client/js/gdpr/table/index.js
+++ client/js/gdpr/table/index.js
@@ -1,13 +1,12 @@
 export class GdprTable {
   /**
+   * @param {HTMLDivElement} chart The element that contains the chart.
    * @param {Map<string, number[]>} histories The ranking history for each
    * track.
    * @param {Date[]} dates The date of each history entry.
-   * @param {HTMLTableElement} table The <table> element to use for this table.
-   * @param {HTMLDivElement} chart The element that contains the chart.
    */
-  constructor(histories, dates, table, chart) {
-    this.table = table;
+  constructor(chart, histories, dates) {
+    this.table = document.createElement('table');
     this.chart = chart;
     this.histories = histories;
     this.dates = dates;
@@ -15,6 +14,8 @@ export class GdprTable {
   }
 
   /**
+   * Sets up the GDPR table by (attaching event handlers, etc.)
+   *
    * @private
    */
   setup() {
@@ -22,10 +23,58 @@ export class GdprTable {
   }
 
   /**
+   * Fires when the user hovers over a series on the chart.
+   *
    * @param {CustomEvent} ev The 'series-hit' event object.
    * @private
    */
   onSeriesHit(ev) {
     const {x, y} = ev.detail;
+    const ranking = [];
+
+    for (const [key, history] of this.histories) {
+      // song's rank at this x position (i.e., date)
+      const rank = history[x];
+      // true if this is the song the user hovered over
+      const selected = rank === y;
+
+      if (rank === undefined || rank === null) {
+        continue;
+      }
+
+      ranking.push({key, rank, selected});
+    }
+
+    // sort by rank ascending
+    ranking.sort((a, b) => a.rank < b.rank ? -1 : 1);
+
+    // update the table
+    this.table.innerHTML = '';
+
+    for (const {key, rank, selected} of ranking) {
+      const row = document.createElement('tr');
+      const rankCell = document.createElement('td');
+      const keyCell = document.createElement('td');
+
+      row.classList.add('gdpr-track');
+      row.classList.toggle('gdpr-track-selected', selected);
+
+      rankCell.innerText = `#${rank}`;
+      keyCell.innerText = key;
+
+      row.append(rankCell, keyCell);
+
+      this.table.append(row);
+    }
+  }
+
+  /**
+   * Places this table in `container`.
+   *
+   * @param {HTMLElement} container The container for this GDPR table.
+   * @public
+   */
+  mount(container) {
+    container.append(this.table);
   }
 }
