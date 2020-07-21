d93f25f00f72691d41b5c46b4c6f0e332282b526
Grace Tian <gracetian6@gmail.com>
2020-07-20 21:47:44 -0400

    chart.js chart

--- client/js/genre.js
+++ client/js/genre.js
@@ -35,6 +35,7 @@ async function displayMusicGenre() {
 
   const genreCount = await response.text();
   genreBlock.innerHTML = genreCount;
+  displayChart();
 }
 
 /**
@@ -49,3 +50,25 @@ function updateNumVids() {
 }
 
 updateNumVids();
+
+function displayChart() {
+  const ctx = document.getElementById('vis').getContext('2d');
+  const chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'line',
+
+    // The data for our dataset
+    data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [{
+            label: 'My First dataset',
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: [0, 10, 5, 2, 20, 30, 45]
+        }]
+    },
+
+    // Configuration options go here
+    options: {}
+  });
+}
