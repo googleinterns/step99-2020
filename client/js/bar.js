653af05db9130bf939cb8999905507b73f90d5db
Grace Tian <gracetian6@gmail.com>
2020-07-23 17:00:53 -0400

    added comments

--- client/js/bar.js
+++ client/js/bar.js
@@ -1,12 +1,12 @@
 /* constructs bar graph from scratch */
 
-const textBlock = document.getElementById('bar-text');
+const barTextContainer = document.getElementById('bar-text');
 const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
 svg.setAttribute('viewBox', '0 0 100 100'); // change height in css
 document.getElementById('graph').appendChild(svg);
 
 // textBlock should align vertically with bar chart
-textBlock.setAttribute('height', svg.getBoundingClientRect().height);
+barTextContainer.setAttribute('height', svg.getBoundingClientRect().height);
 
 // hard coded data for bar chart for now
 const counts = [1, 3, 1, 2];
@@ -14,17 +14,21 @@ const maxCount = 3;
 const genres = ['Pop Music', 'Other Music', 'Electronic Music', 'Music of Latin America'];
 
 const GRAPH_HEIGHT = 100;
-const GRAPH_HEIGHT_CONTAINER = GRAPH_HEIGHT / counts.length;
+const BAR_CONTAINER_HEIGHT = GRAPH_HEIGHT / counts.length;
 
+// height of each category div must equal bar thickness
+const CATEGORY_HEIGHT =
+  BAR_CONTAINER_HEIGHT / 100 * barTextContainer.getAttribute('height');
+
+// bar container contains a bar. It is composed of bar padding and fill
 const BAR_PERCENT_FILL = 0.8;
-const BAR_PERCENT_PADDING = (1 - BAR_PERCENT_FILL) / 2;
-const BAR_THICKNESS = BAR_PERCENT_FILL * GRAPH_HEIGHT_CONTAINER;
-const GRAPH_TOP_PADDING = BAR_PERCENT_PADDING * GRAPH_HEIGHT_CONTAINER;
+const BAR_PERCENT_PADDING = 1 - BAR_PERCENT_FILL;
+const BAR_THICKNESS = BAR_PERCENT_FILL * BAR_CONTAINER_HEIGHT;
+const BAR_UNIT_LENGTH = 100 / maxCount;
 
-const GRAPH_LEFT_PADDING = 5;
-const UNIT_BAR_LENGTH = 100 / maxCount;
-
-const CATEGORY_HEIGHT = GRAPH_HEIGHT_CONTAINER / 100 * textBlock.getAttribute('height');
+// top graph padding depends on bar padding
+const GRAPH_TOP_PADDING = (BAR_PERCENT_PADDING / 2) * BAR_CONTAINER_HEIGHT;
+const GRAPH_LEFT_PADDING = 2.5;
 
 for (let i = 0; i < counts.length; i++) {
   const g = document.createElementNS('http://www.w3.org/2000/svg', 'g');
@@ -33,14 +37,14 @@ for (let i = 0; i < counts.length; i++) {
 
   const bar = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
   g.appendChild(bar);
-  bar.setAttribute('width', UNIT_BAR_LENGTH * counts[i]);
+  bar.setAttribute('width', BAR_UNIT_LENGTH * counts[i]);
   bar.setAttribute('height', BAR_THICKNESS);
   bar.setAttribute('x', GRAPH_LEFT_PADDING);
-  bar.setAttribute('y', GRAPH_TOP_PADDING + GRAPH_HEIGHT_CONTAINER * i);
+  bar.setAttribute('y', GRAPH_TOP_PADDING + BAR_CONTAINER_HEIGHT * i);
 
-  const textContainer = document.createElement('div');
-  textBlock.appendChild(textContainer);
-  textContainer.setAttribute('class', 'category');
-  textContainer.style.height = CATEGORY_HEIGHT + 'px';
-  textContainer.textContent = genres[i];
+  const category = document.createElement('div');
+  barTextContainer.appendChild(category);
+  category.setAttribute('class', 'category');
+  category.style.height = CATEGORY_HEIGHT + 'px';
+  category.textContent = genres[i];
 }
