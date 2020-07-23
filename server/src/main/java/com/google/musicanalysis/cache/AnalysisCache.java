4e3d999a94b761f749598c90b2adaebdeb365314
Seun <seunomonije@icloud.com>
2020-07-23 19:00:50 +0000

    reorganized with timestamps

--- server/src/main/java/com/google/musicanalysis/cache/AnalysisCache.java
+++ server/src/main/java/com/google/musicanalysis/cache/AnalysisCache.java
@@ -30,17 +30,15 @@ enum FileStatus {
 
 public class AnalysisCache {
 
-  private HashMap<String, AnalysisGroup> responseMap;
+  private HashMap<String, CacheValue> cacheMap;
   Cipher cipher;
 
   public AnalysisCache() throws NoSuchAlgorithmException, NoSuchPaddingException {
-    this.responseMap = new HashMap<String, AnalysisGroup>();
+    this.cacheMap = new HashMap<String, CacheValue>();
     this.cipher = Cipher.getInstance("Blowfish");
   }
 
   public void open() throws InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
-    HashMap<String, AnalysisGroup> responseData = null;
-
     FileStatus file = createFile(); // this will have use later.
 
     try {
@@ -50,14 +48,13 @@ public class AnalysisCache {
               new BufferedInputStream(new FileInputStream("cachedData.txt")), cipher);
       ObjectInputStream inData = new ObjectInputStream(inFile);
       SealedObject encodedResponse = (SealedObject) inData.readObject();
-      responseMap = (HashMap<String, AnalysisGroup>) encodedResponse.getObject(cipher);
+      cacheMap = (HashMap<String, CacheValue>) encodedResponse.getObject(cipher);
       inData.close();
       inFile.close();
     } catch (IOException e) {
       e.printStackTrace();
       return;
     } catch (ClassNotFoundException c) {
-      System.out.println("Analysis Group class not found");
       c.printStackTrace();
       return;
     } 
@@ -66,7 +63,7 @@ public class AnalysisCache {
   public void close() throws InvalidKeyException, IllegalBlockSizeException {
     try {
       cipher.init(Cipher.ENCRYPT_MODE, generateKey());
-      SealedObject sealedObject = new SealedObject(this.responseMap, cipher);
+      SealedObject sealedObject = new SealedObject(this.cacheMap, cipher);
       CipherOutputStream outFile =
           new CipherOutputStream(
               new BufferedOutputStream(new FileOutputStream("cachedData.txt")), cipher);
@@ -80,16 +77,16 @@ public class AnalysisCache {
   }
 
   public void add(String requestUrl, AnalysisGroup responseData) {
-    this.responseMap.put(requestUrl, responseData);
+    this.cacheMap.put(requestUrl, new CacheValue(responseData));
   }
 
-  public AnalysisGroup search(String requestUrl) {
+  public CacheValue search(String requestUrl) {
     // map.get() returns null if not match
-    return this.responseMap.get(requestUrl);
+    return this.cacheMap.get(requestUrl);
   }
 
   public void delete(String requestUrl) {
-    this.responseMap.remove(requestUrl);
+    this.cacheMap.remove(requestUrl);
   }
 
   private FileStatus createFile() {
