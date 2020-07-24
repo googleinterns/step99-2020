ebcb0b0385eca05cd120776884c9ad1fa1b46598
Grace Tian <gracetian6@gmail.com>
2020-07-23 20:04:40 -0400

    unindent doc string

--- server/src/main/java/com/google/musicanalysis/site/YoutubeGenres.java
+++ server/src/main/java/com/google/musicanalysis/site/YoutubeGenres.java
@@ -20,11 +20,11 @@ public class YoutubeGenres {
     this.totalLiked = totalLiked;
   }
 
-    /**
-   * parses through youtube liked videos json array,
-   * updates hash map to contain frequency count of each music genre
-   * @param videos json array of youtube liked videos
-   */
+/**
+ * parses through youtube liked videos json array,
+ * updates hash map to contain frequency count of each music genre
+ * @param videos json array of youtube liked videos
+ */
   protected void calculateMusicCount(JsonArray videos) {
     for (int i = 0; i < videos.size(); i++) {
         JsonObject video = videos.get(i).getAsJsonObject();
@@ -69,13 +69,13 @@ public class YoutubeGenres {
     return;
   }
 
-  /**
-   * checks whether topic is categorized as music
-   * and whether it has other 
-   * @param topic identifies youtube video category e.g. Knowledge or Pop music
-   * @return 0 is topic is "Music", 1 if topic is a specific music category (Pop Music), 
-   *        -1 if topic is not music
-   */
+/**
+ * checks whether topic is categorized as music
+ * and whether it has other 
+ * @param topic identifies youtube video category e.g. Knowledge or Pop music
+ * @return 0 is topic is "Music", 1 if topic is a specific music category (Pop Music), 
+ *        -1 if topic is not music
+ */
   private int getMusicCategory(String topic) {
     if (topic.equals("Music")) {
         return 0;
@@ -94,11 +94,11 @@ public class YoutubeGenres {
     return -1;
   }
 
-  /**
-   * updates this.genreData HashMap with new genre or count
-   * by checking if key exists
-   * @param topic identifies youtube video music category e.g. Pop music
-   */
+/**
+ * updates this.genreData HashMap with new genre or count
+ * by checking if key exists
+ * @param topic identifies youtube video music category e.g. Pop music
+ */
   private void updateGenre(String topic) {
     int count = this.genreData.containsKey(topic) ? this.genreData.get(topic) : 0;
     this.genreData.put(topic, count + 1);
