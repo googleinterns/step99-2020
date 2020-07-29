c76d641af0a7dc2216c7b4b10348e10b1612b69a
Seun <seunomonije@icloud.com>
2020-07-29 16:19:45 +0000

    fixed up lightbox

--- client/js/analysis.js
+++ client/js/analysis.js
@@ -267,8 +267,33 @@ function createSVGElement(el) {
 /**
  * Toggles the display class for the lightbox
  *
+ * @param {boolean} isLightboxClosed determines whether or not the lightbox is open
  */
-function toggleDisplay() {
-    const target = document.getElementById("info");
-    target.classList.toggle("display");
+function toggleLightboxVisibility(isLightboxClosed) {
+  if (isLightboxClosed === true) {
+    showLightbox();
+  } else if (isLightboxClosed === false) {
+    hideLightbox();
+  } else {
+    alert('Invalid parameter for toggleLightboxVisibility');
+  }
 }
+
+/**
+ * Hides the lightbox
+ *
+ */
+function hideLightbox() {
+  const target = document.getElementById('info');
+  target.classList.add('hide-lightbox');
+}
+
+/**
+ * Shows the lightbox
+ *
+ */
+function showLightbox() {
+  const target = document.getElementById('info');
+  target.classList.remove('hide-lightbox');
+}
+
