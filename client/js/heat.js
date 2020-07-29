a320af350162e11c956dbcb04c7797e63eae72fa
Grace Tian <gracetian6@gmail.com>
2020-07-28 22:06:04 -0400

    fixed width/height formatting

--- client/js/heat.js
+++ client/js/heat.js
@@ -4,40 +4,51 @@ class HeatRow {
    * @param {string} name The day of the first element in the row
    * @param {int[]} data The array of 0s and 1s for heat row colors
    */
-  constructor(name, data) {
-    this.name = name;
+  constructor(data) {
     this.data = data;
   }
 }
 
 const DEFAULT_DATA = [0, 0, 1, 1, 1];
 const DEFAULT_NAME = 'Metric';
-const DEFAULT_NUM_ROWS = 10;
+const TOTAL_LIKED = 50;
+const DEFAULT_NUM_ROWS = 10; // do square root
 
-const row = new HeatRow(DEFAULT_NAME, DEFAULT_DATA);
+// [{id: 'ererjeirjei', order: 4}]
+// {
+//   genreData: {PopMusic: 2, OtherMusic: 3},
+//   totalLiked: 10,
+//   totalMusic: 5,
+//   maxGenreCount: 3,
+//   likedMusicHistory: [{id: 'ererjeirjei', order: 4}],
+// }
+const row = new HeatRow(DEFAULT_DATA);
 console.log(row);
 const HeatMapValues = [];
-for (var i = 0; i < DEFAULT_NUM_ROWS; i++) {
+for (let i = 0; i < DEFAULT_NUM_ROWS; i++) {
   HeatMapValues.push(row);
 }
 console.log(HeatMapValues);
 
-var options = {
+const options = {
   series: HeatMapValues,
   chart: {
-    height: 500, // change
+    height: '100%',
+    width: '100%', // doesnt fit
     type: 'heatmap',
+    foreColor: 'white',
+    redrawOnParentResize: true,
+  },
+  tooltip: {
+    enabled: false,
   },
   dataLabels: {
     enabled: false,
   },
   colors: ['#5bc0eb'],
-  title: {
-    text: 'HeatMap Chart (Single color)',
-  },
 };
 
-var chart = new ApexCharts(document.querySelector('#heat-map'), options);
+const chart = new ApexCharts(document.querySelector('#heat-map'), options);
 chart.render();
 
 console.log(typeof options);
